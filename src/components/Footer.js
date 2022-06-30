5df68b48387597cfa94efd912ea7d92e8980b0cc
Yousae <yousaex24@gmail.com>
2022-06-29 21:04:32 -0500

    fixed footer attributes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,8 +4,8 @@ const Footer = () => {
     return (
         <footer>
             <div className='footer-attributions'>
-                
-                <p><a href="https://icons8.com" target="_blank" rel="noopener noreferrer">Icons by Icons8</a> | Illustration by <a href="https://icons8.com/illustrations/author/5iGu3XwNjbk5">Igor Kapustin</a> from <a href="https://icons8.com/illustrations">Ouch!</a></p>
+                <p>Icons by<a href="https://icons8.com" target="_blank" rel="noopener noreferrer"> Icons8</a> </p>
+                <p>Illustration by <a href="https://icons8.com/illustrations/author/5iGu3XwNjbk5">Igor Kapustin</a> from <a href="https://icons8.com/illustrations">Ouch!</a></p>
             </div>
             <div className='footer-info'>
                 <a class="animated-link" href="https://www.linkedin.com/in/yousaexiong/" target="_blank" rel="noopener noreferrer">LinkedIn</a>
